c03edcd4a616335d5fbf60a4c1b241d9434be122
Shercosta <70560582+Shercosta@users.noreply.github.com>
2022-07-27 10:15:49 +0700

    Achievement section

---
+++ index.js
@@ -0,0 +1,6 @@
+$(".achievementCol").on("mouseover", function() {
+    $(".fa-solid").css("color", "#F1841A")
+})
+$(".achievementCol").on("mouseleave", function() {
+    $(".fa-solid").css("color", "#30318B")
+})
